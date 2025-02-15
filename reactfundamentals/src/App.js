7c1e1815cfcb221e698e84a0c7fb180130e0ff84
JerwinJustin <jerwin.justin@paarabit.com>
2025-02-15 05:06:03 -0500

    jsx -- done

--- reactfundamentals/src/App.js
+++ reactfundamentals/src/App.js
@@ -2,14 +2,19 @@ import React from 'react';
 import './App.css';
 import Greet from './components/Greet';
 import Welcome from './components/Welcome';
+import Hello from './components/hello';
 
 function App() {
-  return (
-    <div className="App">
-      <Greet />
-      <Welcome />
-    </div>
-  );
+//   return (
+//     <div className="App">
+//       {/* <Greet /> */}
+//       {/* <Welcome /> */}
+// <Hello/>
+//     </div>
+//   );
+      return React.createElement('div',null,<h1>Hello Moon</h1>)
+
 }
+
 
 export default App;
