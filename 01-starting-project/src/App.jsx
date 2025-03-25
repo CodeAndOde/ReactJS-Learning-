bdcc81c8b94f950e4837b151c5453637c5ed0b4a
JerwinJustin <jerwin.justin@paarabit.com>
2025-03-25 05:50:30 -0400

    showing data in a dynamic way

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,10 +1,18 @@
+import reactImg from './assets/react-core-concepts.png'
+const reactDescriptions =['Fundamanetal','Crucial','Core'];
+
+function getRandomInt(max){
+  return Math.floor(Math.random()*(max+1));
+}
+
 function Header(){
+  const dynamicDescription = reactDescriptions[getRandomInt(2)];
   return(
     <header>
-    <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+    <img src={reactImg} alt="Stylized atom" />
     <h1>React Essentials</h1>
     <p>
-      Fundamental React concepts you will need for almost any app you are
+      {dynamicDescription} React concepts you will need for almost any app you are
       going to build!
     </p>
   </header>
