36b1d4675e1131722fffde8e2994b77ab505be31
JerwinJustin <jerwin.justin@paarabit.com>
2025-03-25 09:36:46 -0400

    Upt props

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,4 +1,5 @@
 import reactImg from './assets/react-core-concepts.png'
+import componentsImg from './assets/components.png'
 const reactDescriptions =['Fundamanetal','Crucial','Core'];
 
 function getRandomInt(max){
@@ -18,12 +19,36 @@ function Header(){
   </header>
   );
 }
-
+//props
+function CoreConcept(props){
+  return(<li>
+    <img src={props.img} alt="..."/>
+    <h3>{props.title}</h3>
+    <p>{props.description}</p>
+  </li>)
+}
 function App() {
   return (
     <div>
 <Header></Header>
+
       <main>
+        <section id="core-concepts">
+          <h2>Core Concept</h2>
+          <ul>
+        <CoreConcept
+        title="DotNet Developer"
+        description ="The core UI building block."
+        img={componentsImg}
+        />
+              <CoreConcept
+              title="Learner"
+              description = "Hai i am a learner"
+              img={componentsImg}/>
+              <CoreConcept/>
+              <CoreConcept/>
+          </ul>
+        </section>
         <h2>Time to get started!</h2>
       </main>
     </div>
